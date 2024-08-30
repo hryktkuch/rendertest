3e4f53e667bf7e119553328a5e2400e7b4fe99ad
hryktkuch <hightraffic@gmail.com>
2024-08-30 21:24:31 +0900

    test

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    books=[
+        {'title':'Python Basics','author':'Al Sweigart'},
+        {'title':'Python Tricks','author':'Dan Bader'},
+        {'title':'Flask Web Development','author':'Miguel Grinberg'}
+    ]
+    return render_template('index.html',books=books)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run()
