98c349cc8c710c44f0898eecbb9bfea6c89b0a7c
hryktkuch <hightraffic@gmail.com>
2024-08-30 22:40:46 +0900

    test4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 import sqlite3
 DATABASE = 'database.db'
 app = Flask(__name__)
@@ -27,6 +27,18 @@ def form():
     return render_template(
         'form.html'
     )
+
+@app.route('/register', methods=['POST'])
+def register():
+    title = request.form['title']
+    author = request.form['author']
+
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books (title, author) VALUES (?, ?)', (title, author))
+    con.commit()
+    con.close()
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
